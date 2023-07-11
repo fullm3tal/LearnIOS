64c9d4a9456f2fac080c12ef989ceb765b53277c
Bheem Negi <bheem.negi@vts.com>
2023-07-12 05:19:39 +0530

    Initial Commit

---
+++ LearnIOS/LearnIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnIOSApp.swift
+//  LearnIOS
+//
+//  Created by BigStep on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
