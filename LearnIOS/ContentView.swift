34f9e419fa9e5cf02b81724edfac7767d2616fa3
Bheem Negi <bheem.negi@vts.com>
2023-07-12 05:34:26 +0530

    Initial commit

--- LearnIOS/ContentView.swift
+++ LearnIOS/ContentView.swift
@@ -2,18 +2,28 @@
 //  ContentView.swift
 //  LearnIOS
 //
-//  Created by BigStep on 12/07/23.
+//  Created by Bheem on 12/07/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!").padding(100)
+            Text("My name is Fullm3tal")
+            Button("Show Alert") {
+                showingAlert = true
+            }.alert(isPresented: $showingAlert){
+                Alert(title: Text("Important Message"), message: Text("Wear a toast")
+                      , dismissButton: .default(Text("Got it!")))
+            }
         }
         .padding()
     }
